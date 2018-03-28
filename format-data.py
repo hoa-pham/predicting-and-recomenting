9d9fe7821e0eeed96c7d30aa549df7af37e69d0e
hoa-pham <hpham24@gsu.edu>
2018-03-28 17:13:52 -0400

    convertted data to svm array

--- format-data.py
+++ format-data.py
@@ -13,10 +13,14 @@
 #
 import sys
 fh = open("input-data.txt", "r+")
+f = open("formatted-data.txt", "w+")
+
 
 map = {}
 table = []
-#append data to a 2D array table
+
+#appending data to a 2D array table
+i = 0
 for line in fh:
     line = line.replace(" ", "").rstrip('\n').split(",")
     temp = line[0]
@@ -24,6 +28,17 @@ for line in fh:
     del line[-1]
     line.append(temp) 
     table.append(line) 
+    #if i == 10:
+    #    break
+    i+=1
+
+def ave(table, i):
+    r = 0
+    sum = 0
+    for r in table:
+        sum+=table[r][i]
+        r+=1
+    return sum/len(table[0])
 
 #mapping attributes to number
 c = 0
@@ -32,10 +47,10 @@ for col in range(len(table[0])):
     value = 0
     for row in range(len(table)):
         key = table[r][c] 
-        if key == "?":
-            map[key] = 0
         if key.isdigit():
             break
+        if key == "?":
+            map[key] = 0 
         if map.has_key(key):
             r+=1
             continue
@@ -43,5 +58,18 @@ for col in range(len(table[0])):
         value+=1
         r+=1
     c+=1
-print map
+
+#switching data to svm array
+
+for i, row in enumerate(table):
+    for j, col in enumerate(table[i]):
+        if col == "?":
+            continue
+        if not col.isdigit():
+            table[i][j] = map[col]
+        else:
+            table[i][j] = int(col)
+    print row
+
+
 
