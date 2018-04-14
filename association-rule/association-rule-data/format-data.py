6b01715a3db1e6d356fc2abb18aaffb77f581b75
hoa-pham <hpham24@gsu.edu>
2018-04-14 14:49:35 -0400

    fixed bug in format-data.py

--- association-rule/association-rule-data/format-data.py
+++ association-rule/association-rule-data/format-data.py
@@ -8,7 +8,6 @@ text = soup.get_text()
 for line in text:
     file.write(line)
 file.close()
-import sys
 fh = open("input-data.txt", "r+")
 f = open("formatted-data.txt", "w+")
 
@@ -23,16 +22,17 @@ del table[-1]
 
 i=0
 for row in table:
-    if table[i][12] >= 40:
+    if int(table[i][12])<=20:
+        table[i][12] = '$'
+    elif int(table[i][12])>20 and int(table[i][12])<=40:
         table[i][12] = '$$'
     else:
-        table[i][12] = '$'
+        table[i][12] = '$$$'
     i+=1
 
 #writting svm to a file
 for line in table:
     str = ' '.join(line)
     f.write("%s\n"%str)
-sys.exit()
 
 
