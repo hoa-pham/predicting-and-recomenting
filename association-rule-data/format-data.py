de5fc59153a9fc64fa1b45029e01e9f2e409f4b3
hoa-pham <hpham24@gsu.edu>
2018-04-16 20:16:26 -0400

    updated edu-num hour/week and age

--- association-rule-data/format-data.py
+++ association-rule-data/format-data.py
@@ -23,12 +23,32 @@ del table[-1]
 
 i=0
 for row in table:
+#hours per week
     if int(table[i][12])<=20:
-        table[i][12] = '$'
-    elif int(table[i][12])>20 and int(table[i][12])<=40:
-        table[i][12] = '$$'
+        table[i][12] = 'h1'
+    elif int(table[i][12])>20 and int(table[i][12])>=40:
+        table[i][12] = 'h2'
     else: 
-        table[i][12] = '$$$'
+        table[i][12] = 'h3'
+
+#age
+    if int(table[i][0])<=25:
+        table[i][0] = 'a1'
+    elif int(table[i][0])>25 and int(table[i][0])<=40:
+        table[i][0] = 'a2'
+    elif int(table[i][0])>40 and int(table[i][0])<=60:
+        table[i][0] = 'a3'
+    else: 
+        table[i][0] = 'a4'
+
+#education-num
+    if int(table[i][4])<=12:
+        table[i][4] = 'edu1'
+    elif int(table[i][4])>12 and int(table[i][4])<=14:
+        table[i][4] = 'edu2'
+    else: 
+        table[i][4] = 'edu3'
+
     i+=1
 
 #writting svm to a file
